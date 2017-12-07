5bc477587966fea8e576511c775b86cffd987c8e
gdescombes <gael@ucar.edu>
2017-12-07 17:19:29 +0000

    new addition

---
+++ test/executables/TestModelAuxIncrement.cc
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2017 UCAR
+ * 
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+ */
+
+#include "MPASTraits.h"
+#include "RunMPAS.h"
+#include "test/interface/ModelAuxIncrement.h"
+
+int main(const int argc, const char ** argv) {
+  mpas::RunMPAS run(argc, argv);
+  test::ModelAuxIncrement<mpas::MPASTraits> tests;
+  run.execute(tests);
+  return 0;
+};
+
