f4ee29aaf37f6761ae9c59a0c44e7e2b6eba360e
JJ Guerrette <guerrett@ucar.edu>
2019-04-01 11:13:04 -0600

    Handle NaN's for scatter plots, clean up, add more radiance instruments (#89)

--- test/graphics/plot_diag_omaomb.py
+++ test/graphics/plot_diag_omaomb.py
@@ -1,6 +1,5 @@
 from netCDF4 import Dataset
 import os
-import numpy
 import numpy as np
 from copy import deepcopy
 import matplotlib.pyplot as plt
@@ -9,14 +8,18 @@ import matplotlib.axes as maxes
 import fnmatch
 import math
 
-# columns:    var_name    unit_used  abbr.
-vardict = {'air_temperature': ['(K)','T'],
-    'eastward_wind': ['(m/s)','U'],
-    'northward_wind': ['(m/s)','V'],
-    'specific_humidity': ['(kg/kg)','Q'],
-    'refractivity': ['(%)','Ref'],    #plot RMSE of OMB/O and OMA/O; refractivity unit: N-unit
-    'bending_angle': ['(%)','Bnd'],   #plot RMSE of OMB/O and OMA/O; bending_angle unit: rad
-    'brightness_temperature': ['(K)','BT'] }
+# columns: var_name            unit_used   abbr.
+vardict = { \
+    'air_temperature':        [ '(K)',     'T'   ] \
+  , 'eastward_wind':          [ '(m/s)',   'U'   ] \
+  , 'northward_wind':         [ '(m/s)',   'V'   ] \
+  , 'specific_humidity':      [ '(kg/kg)', 'Q'   ] \
+  , 'refractivity':           [ '(%)',     'Ref' ] \
+  , 'bending_angle':          [ '(%)',     'Bnd' ] \
+  , 'brightness_temperature': [ '(K)',     'BT'  ] \
+    }
+    #Note, refractivity: we plot RMSE of OMB/O and OMA/O; refractivity unit: N-unit
+    #Note, bending_angle: we plot RMSE of OMB/O and OMA/O; bending_angle unit: rad
 
 def readdata():
 
@@ -24,7 +27,7 @@ def readdata():
     #print_fmt = 'pdf' #higher fidelity, slower
 
     profile_group  = ['sonde','aircraft','satwind','gnssro']
-    radiance_group = ['amsua_n19']
+    radiance_group = ['amsua_n18','amsua_n19','amsua_metop-a','amsua_metop-b']
     #dummy_group   = ['dummy_obstype1']
 
     all_groups = []
@@ -33,48 +36,51 @@ def readdata():
     #all_groups.append(dummy_group)
     #all_groups.append(['dummy_obstype2'])
 
-    fileprefix = 'obsout_'
-    filesuffix = '_*.nc4'
-    # * in filesuffix is 4-digit processor rank [0000 to XXXX]
+    # Diagnostic oma/omb files located in diagdir
+    diagdir    = '../Data/'
+    diagprefix = 'obsout_'
+    diagsuffix = '_*.nc4'
+    # * in diagsuffix is 4-digit processor rank [0000 to XXXX]
     #npedigits = 4
 
-    # Suffixes for nc variables
-    omb_suf = 'ombg'
-    oma_suf = 'oman'
-    obs_suf = 'ObsValue'
-    qc_suf  = 'EffectiveQC'
+    # Suffixes to required nc variables
+    omb_var = 'ombg'
+    oma_var = 'oman'
+    obs_var = 'ObsValue'
+    qc_var  = 'EffectiveQC' #Only for final outer iteration currently
 
     obsoutfiles = []
-    for files in os.listdir('../Data/'):
+    for files in os.listdir(diagdir):
         #print(files)
-        if fnmatch.fnmatch(files, fileprefix+'*'+filesuffix):   # 1tile
-            obsoutfiles.append('../Data/'+files)
+        if fnmatch.fnmatch(files, diagprefix+'*'+diagsuffix):   # 1tile
+            obsoutfiles.append(diagdir+files)
     #print(obsoutfiles)
 
-    #Group files by experiment_obstype (e.g., 3dvar_aircraft),
-    # where each group contains files from all PE's
-    file_groups = [[]]
+    # Group files by experiment-obstype combination
+    #  (e.g., 3dvar_aircraft), where each group 
+    #  contains files from all PE's
+    exob_groups = [[]]
     for j, file_name in enumerate(obsoutfiles):
-        # file_group_name excludes everything outside the first/final '_'
-        file_group_name =  '_'.join(file_name.split("_")[1:][:-1])
-        for i, file_group in enumerate(file_groups):
-            if file_group_name in file_group:
-                # If file_group with file_group_name exists, add new file to it
-                update_group = file_group
+        # exob_group_name excludes everything outside the first/final '_'
+        exob_group_name =  '_'.join(file_name.split("_")[1:][:-1])
+        for i, exob_group in enumerate(exob_groups):
+            if exob_group_name in exob_group:
+                # If exob_group with exob_group_name exists, add new file to it
+                update_group = exob_group
                 update_group.append(file_name)
-                file_groups[i][:] = update_group
+                exob_groups[i][:] = update_group
                 break
-            elif i == len(file_groups)-1:
-                # If group with file_group_name does not exist, add one
-                new_group = [file_group_name]
+            elif i == len(exob_groups)-1:
+                # If group with exob_group_name does not exist, add one
+                new_group = [exob_group_name]
                 new_group.append(file_name)
-                file_groups.append(new_group)
+                exob_groups.append(new_group)
                 break
-    file_groups = file_groups[1:][:]
+    exob_groups = exob_groups[1:][:]
 
-    # Loop over unique experiment-obstype combinations
-    for file_group in file_groups:
-        expt_obs = file_group[0]
+    # Loop over unique experiment-obstype groups
+    for exob_group in exob_groups:
+        expt_obs = exob_group[0]
         print(expt_obs)
 
         # Determine obstype from expt_obstype string
@@ -91,10 +97,10 @@ def readdata():
            print('obstype not selected, skipping data: '+expt_obs)
            continue
 
-        # Select variables with the suffix omb_suf (required for OMB)
-        nc = Dataset(file_group[1], 'r')
+        # Select variables with the suffix omb_var (required for OMB)
+        nc = Dataset(exob_group[1], 'r')
         varlist = nc.variables.keys()
-        bglist = [var for var in varlist if (var[-4:] == omb_suf)]
+        bglist = [var for var in varlist if (var[-4:] == omb_var)]
 
         # Define a channel list for radiance_group
         if ''.join(obstype) in radiance_group:
@@ -109,9 +115,9 @@ def readdata():
         for ivar, omb in enumerate(bglist):
             varname = ''.join(omb.split("@")[:-1])
             print(varname)
-            obs=''.join(omb.split("@")[:-1])+'@'+obs_suf
-            oma=''.join(omb.split("@")[:-1])+'@'+oma_suf
-            qc = ''.join(omb.split("@")[:-1])+'@'+qc_suf
+            obs=''.join(omb.split("@")[:-1])+'@'+obs_var
+            oma=''.join(omb.split("@")[:-1])+'@'+oma_var
+            qc = ''.join(omb.split("@")[:-1])+'@'+qc_var
             #print("obs=",obs,"omb=",omb,"oma=",oma)
 
             obsnc = np.asarray([])
@@ -120,9 +126,9 @@ def readdata():
             qcnc  = np.asarray([])
             prenc = np.asarray([])
 
-            # Build up arrays in loop over file_group, 
-            # excluding category in file_group[0]
-            for file_name in file_group[1:]:
+            # Build up arrays in loop over exob_group, 
+            # excluding category in exob_group[0]
+            for file_name in exob_group[1:]:
                 nc = Dataset(file_name, 'r')
                 #file_rank = file_name[-(4+npedigits):-4]
 
@@ -139,10 +145,10 @@ def readdata():
 
             #@EffectiveQC, 1: missing; 0: good; 10: rejected by first-guess check
             #keep data @EffectiveQC=0
-            obsnc[numpy.logical_not(qcnc == 0)] = numpy.NaN
-            ombnc[numpy.logical_not(qcnc == 0)] = numpy.NaN
-            omanc[numpy.logical_not(qcnc == 0)] = numpy.NaN
-            ombnc[ombnc < -1.0e+15 ] = numpy.NaN
+            obsnc[np.logical_not(qcnc == 0)] = np.NaN
+            ombnc[np.less(ombnc,-1.0e+15)] = np.NaN
+            ombnc[np.logical_not(qcnc == 0)] = np.NaN
+            omanc[np.logical_not(qcnc == 0)] = np.NaN
 
             if ''.join(obstype) == 'gnssro':
                 ombnc = (ombnc/obsnc)*100
@@ -161,16 +167,16 @@ def readdata():
                 else:
                     bins =   [1050.,950.,850.,750.,650.,550.,450.,350.,250.,150.,50.,0.]
                     binsfory=  [1000.,900.,800.,700.,600.,500.,400.,300.,200.,100.,0]
-                bins = numpy.asarray(bins)
+                bins = np.asarray(bins)
 
                 for j in range(0, len(bins)-1):
                     obsncbin = deepcopy(obsnc)
                     ombncbin = deepcopy(ombnc)
                     omancbin = deepcopy(omanc)
 
-                    obsncbin[numpy.logical_or(prenc <bins[j+1], prenc >=bins[j])] = numpy.NaN
-                    ombncbin[numpy.logical_or(prenc <bins[j+1], prenc >=bins[j])] = numpy.NaN
-                    omancbin[numpy.logical_or(prenc <bins[j+1], prenc >=bins[j])] = numpy.NaN
+                    obsncbin[np.logical_or(prenc <bins[j+1], prenc >=bins[j])] = np.NaN
+                    ombncbin[np.logical_or(prenc <bins[j+1], prenc >=bins[j])] = np.NaN
+                    omancbin[np.logical_or(prenc <bins[j+1], prenc >=bins[j])] = np.NaN
 
                     RMSEomb = np.sqrt(np.nanmean(ombncbin**2))
                     RMSEoma = np.sqrt(np.nanmean(omancbin**2))
@@ -182,23 +188,27 @@ def readdata():
 
                 plotrmsepro(RMSEombs,RMSEomas,binsfory,obsnums,expt_obs,varname,print_fmt)
             else:
+                #Default: generate scatter plots
+
                 # Maximum number of variables/channels per figure
                 maxsubplts = 12
-
                 if ''.join(obstype) in radiance_group:
                     #RADIANCE OBS
                     maxsubplts = 16
                     ch = ''.join(varname.split("_")[-1:])
                     ifig = chlist.index(int(ch)) + 1
-                    shortname = 'BT, ch. '+ch
-                    units = 'K'
+                    dictname = '_'.join(varname.split("_")[:-1])
+                    shortname = ', ch. '+ch
                 else:
                     print('NOTIFICATION: obstype = '+obstype+' not defined, using default scatter plots')
                     #Generic scatter verification for any obstype
                     ## (similar to radiance_group):
                     ifig = ivar
-                    shortname = varname
-                    units = ''
+                    dictname = varname
+                    shortname = ''
+                varval = vardict.get(dictname,['',dictname])
+                units = varval[0]
+                shortname = varval[1] + shortname
 
                 if ivar == 0:
                     # scatter_verification yields 2 figure types
@@ -340,8 +350,8 @@ def scatter_verification(ifig, varname, varunits, ivar, nvars, \
             xlab = 'y'
             ylab = 'h(x)'
         elif iifig == 1:
-            xlab = 'OMB'
-            ylab = 'OMA'
+            xlab = 'h(xb) - y'
+            ylab = 'h(xa) - y'
         else:
             print('WARNING: scatter_verification has no definitions for nfigtypes == ',nfigtypes)
             continue
@@ -355,15 +365,24 @@ def scatter_verification(ifig, varname, varunits, ivar, nvars, \
         if iifig == 0:
             #Add scatter plot for h(x) vs. y
             fname = 'XB_XA_%s'%(EXP_NAME)
-            scatter_one2ones(obs, [obs-omb , obs-oma],
-                             ['x_b' , 'x_a'], True, xlab, ylab,
-                             varname, varunits, ax)
+            stat = scatter_one2ones( \
+                obs, [obs-omb , obs-oma], \
+                ['x_b' , 'x_a'], True, xlab, ylab, \
+                varname, varunits, ax)
         if iifig == 1:
             #Add scatter plot for OMA vs. OMB
             fname = 'OMB_OMA_%s'%(EXP_NAME)
-            scatter_one2ones(omb , [oma],
-                             [], False, xlab, ylab,
-                             varname, varunits, ax)
+            stat = scatter_one2ones( \
+                -omb , [-oma], \
+                [], False, xlab, ylab, \
+                varname, varunits, ax)
+
+        if stat != 0:
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.text(0.5, 0.5, '[NO DATA]',
+                {'color': 'k', 'fontsize': 12}, 
+                ha='center', va='center', transform=ax.transAxes)
 
         if nnfigs > 1: fname=fname+'_%d-of-%d'%(jfig,nnfig)
         fname=fname+'.'+fmt
@@ -399,6 +418,27 @@ def scatter_one2ones(XVAL,YVALS,LEG,show_stats,XLAB,YLAB,VAR_NAME,UNITS,ax):
 #         + linear regressions for each list contained in YVALS
 #         + a one-to-one line
 #================================================================
+    fsize1 = 6.0
+    fsize2 = 4.0
+
+    ax.text(0.03, 0.97 - len(LEG) * 0.125, VAR_NAME,
+        {'color': 'k', 'fontsize': fsize1}, 
+        ha='left', va='top', transform=ax.transAxes)
+
+    if len(XVAL) == 0: 
+        print('WARNING in scatter_one2ones: len(XVAL)==0; skipping this dataset')
+        return 1
+    NVALS = np.asarray([])
+    for i,YVAL in enumerate(YVALS):
+        if len(XVAL) != len(YVAL):
+            print('ERROR: Incorrect usage of scatter_one2ones, YVALS must be list of arrays.')
+            os._exit()
+        not_nan = np.isfinite(XVAL) & np.isfinite(YVAL)
+        NVALS = np.append(NVALS,np.sum(not_nan))
+
+    if np.all(NVALS == 0):
+        print('WARNING in scatter_one2ones: all(XVAL/YVAL) are non-finite; skipping this dataset')
+        return 1
 
     colors = [ \
               [0.0000, 0.4470, 0.7410], \
@@ -411,10 +451,8 @@ def scatter_one2ones(XVAL,YVALS,LEG,show_stats,XLAB,YLAB,VAR_NAME,UNITS,ax):
              ]
     markers = ['*','+','o','.']
     msizes  = [0.5,0.5,0.5, 3 ]
+
     for i,YVAL in enumerate(YVALS):
-        if len(XVAL) != len(YVAL):
-            print('ERROR: Incorrect usage of scatter_one2ones, YVALS must be list of arrays.')
-            os._exit()
         col = colors[np.mod(i,len(colors))]
         mind = np.mod(i,len(markers))
         ax.plot( XVAL, YVAL, markers[mind], color = col, \
@@ -422,26 +460,24 @@ def scatter_one2ones(XVAL,YVALS,LEG,show_stats,XLAB,YLAB,VAR_NAME,UNITS,ax):
 
     if XLAB != '':
         label = XLAB
-        if UNITS != '': label = label+' ('+UNITS+')'
+        if UNITS != '': label = label+' '+UNITS
         ax.set_xlabel(label,fontsize=6)
     if YLAB != '':
         label = YLAB
-        if UNITS != '': label = label+' ('+UNITS+')'
+        if UNITS != '': label = label+' '+UNITS
         ax.set_ylabel(label,fontsize=6)
     if len(LEG) > 0:
        ax.legend(LEG, loc='upper left',fontsize=5)
-
-    fsize1 = 6.0
-    fsize2 = 4.0
-    ax.text(0.03, 0.75, VAR_NAME,
-        {'color': 'k', 'fontsize': fsize1}, 
-        ha='left', va='top', transform=ax.transAxes)
-
 
     ymin, ymax = ax.get_ylim()
     xmin, xmax = ax.get_xlim()
     xymin=min(xmin,ymin)
     xymax=max(xmax,ymax)
+
+    #Could adjust limits/ticks for better aesthetics
+    #round_nmbr = 5
+    #xymin=np.floor(min(xmin,ymin) / round_nmbr) * round_nmbr
+    #xymax=np.ceil(max(xmax,ymax) / round_nmbr) * round_nmbr
 
     # Add linear regression and statistics
     predictor = np.arange(xymin, xymax, (xymax - xymin) / 10.0)
@@ -450,36 +486,31 @@ def scatter_one2ones(XVAL,YVALS,LEG,show_stats,XLAB,YLAB,VAR_NAME,UNITS,ax):
     nline = ''
     for j,YVAL in enumerate(reversed(YVALS)):
         i = len(YVALS) - j - 1
-        p = np.polyfit(XVAL,YVAL,1)
+        if NVALS[j]==0: continue
+        del not_nan
+        not_nan = np.isfinite(XVAL) & np.isfinite(YVAL)
+
+        # Add linear fit to YVAL vs. XVAL
+        p = np.polyfit(XVAL[not_nan],YVAL[not_nan],1)
         predicted = p[0] * predictor + p[1]
         col0 = colors[np.mod(i,len(colors))]
 #        bright = 0.5
 #        col = bright * np.asarray([1.,1.,1.]) + (1. - bright) * np.asarray(col0)
         dimmer = 0.35
         col = (1. - dimmer) * np.asarray(col0)
-
         ax.plot( predictor, predicted, '-', color = col, lw=1.2 )
-        stat = 'slope: %0.2f  '%(p[0])
 
+        # Add statistics for YVAL vs. XVAL
+        stat = 'N = %d\nslope: %0.2f  '%(NVALS[j],p[0])
         if show_stats:
-            RMSE = np.sqrt( np.sum( np.square(YVAL - XVAL) ) / len(XVAL) )
-            BIAS = np.sum( YVAL - XVAL ) / len(XVAL)
+            RMSE = np.sqrt( np.sum( np.square(YVAL[not_nan] - XVAL[not_nan]) ) / NVALS[j] )
+            BIAS = np.sum( YVAL[not_nan] - XVAL[not_nan] ) / NVALS[j]
             stat = stat+'\nRMSE: %0.2f \nBIAS: %0.2f'%(RMSE,BIAS)
         ax.text(tx, ty, stat+nline, \
             {'color': col, 'fontsize': fsize2}, 
             ha='right', va='bottom', backgroundcolor=[1,1,1,0.2], \
             clip_on=True, transform=ax.transAxes)
         nline = nline + ''.join('\n' * stat.count('\n')) + '\n'
-
-    ax.text(tx, ty, 'N = %d'%(len(XVAL))+nline,
-        {'color': 'k', 'fontsize': fsize2}, 
-        ha='right', va='bottom', transform=ax.transAxes)
-
-
-    #Could try to adjust limits/ticks for better aesthetics
-    #round_nmbr = 5
-    #xymin=np.floor(min(xmin,ymin) / round_nmbr) * round_nmbr
-    #xymax=np.ceil(max(xmax,ymax) / round_nmbr) * round_nmbr
 
     ax.plot([xymin,xymax],[xymin,xymax],'k--',lw=0.5)
     ticks = ax.get_yticks()
@@ -495,6 +526,7 @@ def scatter_one2ones(XVAL,YVALS,LEG,show_stats,XLAB,YLAB,VAR_NAME,UNITS,ax):
     ax.set_xlim(xymin,xymax)
     ax.set_ylim(xymin,xymax)
     ax.grid()
+    return 0
 
 def main():
     readdata()
