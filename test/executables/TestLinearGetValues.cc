ca25c2d795f29cc49776b690dd9c74006d35f2df
Steven Vahl <42982505+svahl991@users.noreply.github.com>
2020-04-22 09:43:42 -0600

    Add GetValues ctests (#298)
    TYPE: feature
    
    KEYWORDS: getvalues lineargetvalues test
    
    DESCRIPTION OF CHANGES: This PR adds the class ctests for the new GetValues and LinearGetValues classes
    
    LIST OF MODIFIED FILES:
    M test/CMakeLists.txt
    A test/executables/TestGetValues.cc
    A test/executables/TestLinearGetValues.cc
    A test/testinput/getvalues.yaml
    A test/testinput/lineargetvalues.yaml
    
    TESTS CONDUCTED: Tested with gnu-openmpi on Cheyenne. The LinearGetValues test currently fails. This will be fixed in a follow-on bugfix PR.

---
+++ test/executables/TestLinearGetValues.cc
@@ -0,0 +1,17 @@
+/*
+ * (C) Copyright 2017 UCAR
+ * 
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+ */
+
+#include "MPASTraits.h"
+#include "RunMPAS.h"
+#include "test/interface/LinearGetValues.h"
+
+int main(int argc,  char ** argv) {
+  mpas::RunMPAS run(argc, argv);
+  test::LinearGetValues<mpas::MPASTraits> tests;
+  return run.execute(tests);
+}
+
