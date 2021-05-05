3324e4c5a7caf5eb154772a3ba84d3da21146e9b
JJ Guerrette <guerrett@ucar.edu>
2021-05-05 12:56:24 -0600

    Process concatenated IODA obs, geoval, and ydiag files (#563)
    TYPE: new feature
    
    DESCRIPTION OF CHANGES: The changes in this PR enable the observation-space statistics package to handle ObsSpace, GeoVaLs, and ObsDiagnostics files that are previously concatenated across processors.  This is crucial for high processor counts that bog down the file reading step of this process.  The file concatenation is handled by an external tool.
    
    ISSUE:
    Closes #562 
    
    LIST OF MODIFIED FILES:
    M       graphics/JediDB.py
    
    TESTS CONDUCTED: tried for a single cycle.  Still works for non-concatenated files.  Copied the 0000 processor files for AHI to `geoval_hofx_ahi_himawari8.nc4`,  `obsout_hofx_ahi_himawari8.nc4`, `ydiags_hofx_ahi_himawari8.nc4`.  Count is reduced in the new `stats_hofx_ahi_himawari8.nc4`, corresponding to tricking the program to handle a file from a single processor as though all processors are contained in the one file.

--- graphics/JediDB.py
+++ graphics/JediDB.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 import config as conf
 from copy import deepcopy
 import glob
@@ -19,11 +20,32 @@ MAXINT32  = np.int32(1e9)
 MAXFLOAT  = np.float32(1.e12)
 MAXDOUBLE = np.float64(1.e12)
 
+def getIODAFileRank(pathPlusFile):
+    # pathPlusFile = PATH/fileName
+    # fileName = prefix_suffix.fileExt
+    # suffix may or may not be a numerical value indicating processing element rank
+    # return the rank if any
+
+    # fileName excludes the path
+    fileName = pathPlusFile.split('/')[-1]
+
+    # fileParts as list, removing extension (after '.') first
+    fileParts = (fileName.split('.')[0]).split('_')
+
+    rank = fileParts[-1]
+
+    # check if suffix is numerical, indicating the rank number
+    if not pu.isint(rank):
+        return 'none'
+    else:
+        return rank
+
+def IODAFileIsRanked(pathPlusFile):
+    return pu.isint(getIODAFileRank(pathPlusFile))
+
 class JediDB:
     '''This class provides access to UFO feedback files.'''
-    def __init__(self,data_path=default_path,fileExt='nc4'
-        ,osKeySelect=[]
-        ):
+    def __init__(self,data_path=default_path, fileExt='nc4', osKeySelect=[]):
         # data_path: location of feedback files
         # fileExt: extention of feedback files
         # osKeySelect (optional): allows for the selection of particular osKey's
@@ -43,52 +65,80 @@ class JediDB:
         for key, prefix in fPrefixes.items():
             self.filePrefixes[key] = prefix
 
-        # catalog all relevant files
-        allFiles = {}
-        for fileType, prefix in self.filePrefixes.items():
-            allFiles[fileType] = []
-            for File in glob.glob(data_path+'/'+prefix+'*.'+fileExt):
-                allFiles[fileType].append(File)
+        # Group all files by experiment-ObsSpace combination
+        # Two possible formats for multi-processor or single-processor/concatenated
+        #  multi-processor: self.filePrefixes[fileType]+"_"+osKey+"_"+rank+"."+fileExt
+        #  single-processor: self.filePrefixes[fileType]+"_"+osKey+"."+fileExt
 
-        # Group files by experiment-ObsSpace combination
-        #  Must fit the format filePrefixes[fileType]+"_"+osKey+"_"+PE+"."+fileExt,
-        #  where osKey includes strings associated with both the experiment and ObsSpaceName
-        #  and where each group contains files from all PE's,
-        #  which is the 4-digit processor rank [0000 to XXXX]
+        #  osKey includes strings associated with both the experiment and ObsSpaceName
+        #  rank, when present, is the four digit processor element rank.  If a file matching
+        #  the single-processor format is present, it will be selected in place of the other
+        #  files.
+        #
         #  examples:
-        #   +  obsout_3dvar_sondes_0000.nc4, where
-        # osKey = "3dvar_sondes"
+        #   +  obsout_hofx_sondes_0000.nc4 OR
+        #   +  obsout_hofx_sondes.nc4
+        # osKey = "hofx_sondes"
         #
-        #   +  obsout_3denvar_bumploc_amsua_n19_0000.nc4, where
-        # osKey = "3denvar_bumploc_amsua_n19"
+        #   +  obsout_variational_amsua_n19_0000.nc4 OR
+        #   +  obsout_variational_amsua_n19.nc4
+        # osKey = "variational_amsua_n19"
         #
-        #   +  obsout_3dvar_bumpcov_bumploc_amsua_n19_0000.nc4, where
+        #   +  obsout_3dvar_bumpcov_bumploc_amsua_n19_0000.nc4 OR
+        #   +  obsout_3dvar_bumpcov_bumploc_amsua_n19.nc4
         # osKey = "3dvar_bumpcov_bumploc_amsua_n19"
         #
-        #   +  obsout_abi_g16_0000.nc4, where
+        #   +  obsout_abi_g16_0000.nc4 OR
+        #   +  obsout_abi_g16.nc4
         # osKey = "abi_g16"
 
-        self.Files = {}
-        for fileType, files in allFiles.items():
-            for fileName in files:
-                # fileBase excludes the path
-                fileBase = fileName.split("/")[-1]
+        # note: when obsFKey (ObsSpace) files are concatenated into a single file, the
+        #   optional geoval and ydiag files must also be concatenated in order to
+        #   preserve locations ordering
 
-                # osKey excludes everything outside the first/final '_'
-                osKey =  '_'.join(fileBase.split("_")[1:][:-1])
+        allFiles = {}
+        for fileType, prefix in self.filePrefixes.items():
+            allFiles[fileType] = defaultdict(list)
+            for pathPlusFile in glob.glob(data_path+'/'+prefix+'*.'+fileExt):
+                # fileName excludes the path
+                fileName = pathPlusFile.split('/')[-1]
+
+                # fileParts as list, removing extension (after '.') first
+                fileParts = (fileName.split('.')[0]).split('_')
+
+                if not IODAFileIsRanked(pathPlusFile):
+                    # osKey includes everything after the first '_'
+                    osKey = '_'.join(fileParts[1:])
+                else:
+                    # osKey includes everything within the first/final '_'
+                    osKey =  '_'.join(fileParts[1:][:-1])
+
+                allFiles[fileType][osKey].append(pathPlusFile)
+
+        # + transpose files for easy access by osKey
+        # + remove PE-ranked files when concatenated file is present
+        self.Files = {}
+        for fileType, osKeys in allFiles.items():
+            for osKey, files in osKeys.items():
                 if osKey not in self.Files:
-                    self.Files[osKey] = {}
-                if fileType not in self.Files[osKey]:
-                    self.Files[osKey][fileType] = []
-                self.Files[osKey][fileType].append(fileName)
+                  self.Files[osKey] = defaultdict(list)
+                for pathPlusFile in files:
+                    if not IODAFileIsRanked(pathPlusFile):
+                        # for non-numerical suffix assume that this one file includes ALL locations
+                        self.Files[osKey][fileType] = [pathPlusFile]
+                        break
+                    else:
+                        # otherwise, append this file to list
+                        self.Files[osKey][fileType].append(pathPlusFile)
+        del allFiles
 
         # error checking
         self.nObsFiles = {}
         self.exists = {}
         self.loggers = {}
-        for osKey, fileGroups in self.Files.items():
+        for osKey, fileTypes in self.Files.items():
             self.loggers[osKey] = logging.getLogger(__name__+'.'+osKey)
-            nObsFiles = len(fileGroups.get(obsFKey,[]))
+            nObsFiles = len(fileTypes.get(obsFKey,[]))
             # force crash when there are no obsFKey files available for an osKey
             if nObsFiles < 1:
                 self.loggers[osKey].error('''
@@ -98,11 +148,11 @@ class JediDB:
             self.nObsFiles[osKey] = nObsFiles
             self.exists[osKey] = {}
             self.exists[osKey][obsFKey] = True
-            # force crash when # of non-obs files is > 1 but ~= # of obsFKey files
+            # force crash when # of non-obs files is > 0 but != # of obsFKey files
             for key, prefix in self.filePrefixes.items():
                 if key == obsFKey: continue
                 self.exists[osKey][key] = False
-                nFiles = len(fileGroups.get(key,[]))
+                nFiles = len(fileTypes.get(key,[]))
                 if nFiles > 0 and nFiles < nObsFiles:
                     self.loggers[osKey].error('''
                                There are not enough '''+key+'''
@@ -136,15 +186,22 @@ class JediDB:
                 self.ObsSpaceName[osKey] = deepcopy(ObsSpaceName)
                 self.ObsSpaceGroup[osKey] = deepcopy(ObsSpaceInfo['DiagSpaceGrp'])
 
-        # sort remaining files by PE
-        for osKey, fileGroups in self.Files.items():
-            for fileType, files in fileGroups.items():
-                PEs = []
+        # sort remaining files by rank
+        for osKey, fileTypes in self.Files.items():
+            for fileType, files in fileTypes.items():
+                ranks = []
                 for fileName in files:
-                    PE = ''.join(fileName.split("_")[-1].split(".")[0])
-                    PEs.append(int(PE))
+                    rank = getIODAFileRank(fileName)
+                    if pu.isint(rank):
+                        ranks.append(int(rank))
+                    elif len(files) == 1:
+                        ranks = [1]
+                        break
+                    else:
+                        self.loggers[osKey].error('too many files chosen for a concatenated ObsSpace=> '+fileType)
+
                 indices = list(range(len(files)))
-                indices.sort(key=PEs.__getitem__)
+                indices.sort(key=ranks.__getitem__)
                 self.Files[osKey][fileType] = \
                     list(map(files.__getitem__, indices))
 
@@ -152,22 +209,13 @@ class JediDB:
 
     # TODO: add function that concatenates files together, then
     # TODO: replaces the original Files and/or Handles
-    #
-    # TODO: first step would be to check if concatenated version exists,
-    # TODO: then only initialize a single Handle for combined file
-    #
-    # TODO: enable __init__ function to check for concatenated version
-    # TODO: of osKey files
-
 
     def initHandlesNC(self,osKey):
     #initialize Handles for osKey
     # osKey (string) - experiment-ObsSpace key
         self.loggers[osKey].info('Initializing UFO file handles...')
 
-        self.Handles[osKey] = {}
-        for fileType in self.filePrefixes.keys():
-            self.Handles[osKey][fileType] = []
+        self.Handles[osKey] = defaultdict(list)
 
         for fileType, files in self.Files[osKey].items():
             if len(files) == self.nObsFiles[osKey]:
