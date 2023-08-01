42276ff9f1e2709e7229fb54262bc4342b09e97a
Byoung-Joo (BJ) Jung <33296274+byoung-joo@users.noreply.github.com>
2023-08-01 11:20:04 -0600

    Add generic PseudoModel and revise the FGAT and HofX tests. (#894)
    TYPE: enhancement
    
    DESCRIPTION OF CHANGES: 
    
     * This PR adds a generic PseudoModel from oops, which enable the reading of pre-generated model forecast file, rather than actually running the model within "window".
     * This PR revises the `FGAT` and `HofX` tests.
       * `4dfgat.yaml` : renamed from old 3dfgat.yaml. uses `4D-Var` cost function with Identity linear model.
       * `3dfgat.yaml` : uses `3D-FGAT` cost function. **For the first outer loop**, it runs the MPAS model forecast initialized at T-3h to calculate the H(x_b), Analysis increment is added to the center of assimilation window.
       * `3dfgat_pseudo.yaml` : Based on 3dfat.yaml. Rather than running the MPAS model forecast, reads the pre-generated MPAS forecast files by using OOPS’s generic `PseudoModel`.
       * `hofx4d.yaml` : renamed from old hofx.yaml
       * `hofx4d_pseudo.yaml` : Based on hofx4d.yaml.  Rather than running the MPAS model forecast, reads the pre-generated MPAS forecast files by using OOPS’s generic `PseudoModel`.
     * More details and discussions on the `3D-FGAT` cost function can be found at https://github.com/JCSDA-internal/oops/pull/2195.
     * See https://github.com/JCSDA-internal/oops/blob/develop/src/oops/generic/PseudoModel.h for `PseudoModel`.
    
    LIST OF MODIFIED FILES: 
    M       src/mains/mpasHofX.cc
    M       src/mains/mpasVariational.cc
    M       test/CMakeLists.txt
    M       test/testinput/3dfgat.yaml
    A       test/testinput/3dfgat_pseudo.yaml
    A       test/testinput/4dfgat.yaml
    R088    test/testinput/hofx.yaml        test/testinput/hofx4d.yaml
    A       test/testinput/hofx4d_pseudo.yaml
    M       test/testoutput/3dfgat.ref
    A       test/testoutput/3dfgat_pseudo.ref
    A       test/testoutput/4dfgat.ref
    R100    test/testoutput/hofx.ref        test/testoutput/hofx4d.ref
    A       test/testoutput/hofx4d_pseudo.ref
    
    TESTS CONDUCTED: Tested on Cheyenne.

--- src/mains/mpasHofX.cc
+++ src/mains/mpasHofX.cc
@@ -5,16 +5,18 @@
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
  */
 
-#include <oops/runs/HofX4D.h>
-#include <oops/runs/Run.h>
+#include "oops/generic/instantiateModelFactory.h"
+#include "oops/runs/HofX4D.h"
+#include "oops/runs/Run.h"
 
-#include <ufo/instantiateObsFilterFactory.h>
-#include <ufo/ObsTraits.h>
+#include "ufo/instantiateObsFilterFactory.h"
+#include "ufo/ObsTraits.h"
 
 #include "mpasjedi/Traits.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
+  oops::instantiateModelFactory<mpas::Traits>();
   ufo::instantiateObsFilterFactory();
   oops::HofX4D<mpas::Traits, ufo::ObsTraits> hofx;
   return run.execute(hofx);
--- src/mains/mpasVariational.cc
+++ src/mains/mpasVariational.cc
@@ -5,23 +5,24 @@
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
  */
 
-#include <oops/runs/Run.h>
-#include <oops/runs/Variational.h>
+#include "oops/generic/instantiateModelFactory.h"
+#include "oops/runs/Run.h"
+#include "oops/runs/Variational.h"
 
-#include <saber/oops/instantiateCovarFactory.h>
-#include <saber/oops/instantiateLocalizationFactory.h>
+#include "saber/oops/instantiateCovarFactory.h"
+#include "saber/oops/instantiateLocalizationFactory.h"
 
-#include <ufo/instantiateObsFilterFactory.h>
-#include <ufo/ObsTraits.h>
+#include "ufo/instantiateObsFilterFactory.h"
+#include "ufo/ObsTraits.h"
 
 #include "mpasjedi/Traits.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
+  oops::instantiateModelFactory<mpas::Traits>();
   saber::instantiateCovarFactory<mpas::Traits>();
   saber::instantiateLocalizationFactory<mpas::Traits>();
   ufo::instantiateObsFilterFactory();
   oops::Variational<mpas::Traits, ufo::ObsTraits> var;
   return run.execute(var);
 }
-
