caa32ad1d3abb468abfd36e6778def1e97322da4
Zhiquan (Jake) Liu <liuz@ucar.edu>
2024-07-17 10:27:31 -0600

    Update script for plotting cost function and gradient norm reduction
    TYPE: enhancement
    
    DESCRIPTION OF CHANGES:
    The updated script plots 4 curves (J, Jo, Jb, and gradient norm reduction) in one single panel figure. This is based upon the script used in the September 2023 tutorial, and then further revised by Hejun Xie, a graduate student visitor from Zhejiang University, China.
    Note that the jedi.log file is assumed to read in the same directory as the plotting script.
    
    LIST OF MODIFIED FILES:
    M       graphics/standalone/plot_cost_grad.py
    
    TESTS CONDUCTED: no need as this is for graphics.

--- graphics/standalone/plot_cost_grad.py
+++ graphics/standalone/plot_cost_grad.py
@@ -33,61 +33,90 @@ def integers(a, b):
 
 def readdata():
 
-    dalogfiles = []
-    for files in os.listdir('../testoutput/'):
-        if fnmatch.fnmatch(files, '?d*.run'):
-            dalogfiles.append('../testoutput/'+files)
-            #print(dalogfiles)
+    dalogfile = 'jedi.log'
+    file_name = 'costgrad.txt'
 
-    for dalogfile in dalogfiles:
-        print('check dalogfile=',dalogfile,dalogfile[14:][:-4])
-        file_name = 'costgrad_'+dalogfile[14:][:-4]+'.txt'
+    exists = os.path.isfile(file_name)
+    if exists:
+        os.system('rm '+ file_name)
 
-        exists = os.path.isfile(file_name)
-        if exists:
-            os.system('rm '+ file_name)
+    #grep iterations and cost function.
+    cmd = 'grep "'+VAR1+': J " '+ dalogfile +' \
+        | grep -o -P "(\(\K[^\)]+)|(=\K.+)"  |paste -d " " - - > cost.txt'
+    os.system(cmd)
 
-        #grep iterations and cost function.
-        cmd = 'grep "'+VAR1+': J " '+ dalogfile +' \
-            | grep -o -P "(\(\K[^\)]+)|(=\K.+)"  |paste -d " " - - > cost.txt'
-        os.system(cmd)
+    cmd = 'grep "'+VAR2+'" '+ dalogfile +' \
+        | grep -o -P "=\K.+" > gradorig.txt'
+    os.system(cmd)
 
-        cmd = 'grep "'+VAR2+'" '+ dalogfile +' \
-            | grep -o -P "=\K.+" > gradorig.txt'
-        os.system(cmd)
+    cmd = 'grep "'+VAR1+': Jb" '+ dalogfile +' \
+        | grep -o -P "=\K.+"  > costJb.txt'
+    os.system(cmd)
 
-        nLineCost = int(os.popen('wc -l < cost.txt').read())
-        nLineGrad = int(os.popen('wc -l < gradorig.txt').read())
+    cmd = 'grep "'+VAR1+': JoJc" '+ dalogfile +' \
+        | grep -o -P "=\K.+"  > costJoJc.txt'
+    os.system(cmd)
 
-        #some outputs included GMRESR, remove 'Gradient reduction' or 'Norm reduction' from it.
-        if nLineCost !=  nLineGrad:
-            cmd = 'head -n '+str(nLineCost)+' gradorig.txt > grad.txt'
-        else:
-            cmd = 'cp gradorig.txt grad.txt'
-        os.system(cmd)
-        #combine cost and grad:
-        cmd = 'paste cost.txt grad.txt > '+ file_name +' ; rm cost.txt grad.txt gradorig.txt'
-        os.system(cmd)
+    nLineCost = int(os.popen('wc -l < cost.txt').read())
+    nLineGrad = int(os.popen('wc -l < gradorig.txt').read())
 
-        alist = open(file_name).read().split()
-        iters  = numpy.asarray(alist[0::3])  
-        cost = numpy.asarray(alist[1::3]).astype(np.float)
-        grad = numpy.asarray(alist[2::3]).astype(np.float)
-        forx=integers(1,len(iters)) 
-        plot(forx,cost,iters,VAR1,dalogfile[14:])
-        plot(forx,grad,iters,VAR2,dalogfile[14:])
+    #some outputs included GMRESR, remove 'Gradient reduction' or 'Norm reduction' from it.
+    if nLineCost !=  nLineGrad:
+        cmd = 'head -n '+str(nLineCost)+' gradorig.txt > grad.txt'
+    else:
+        cmd = 'cp gradorig.txt grad.txt'
+    os.system(cmd)
+    #combine cost and grad:
+    cmd = 'paste cost.txt grad.txt costJb.txt costJoJc.txt > '+ file_name +' ; rm cost.txt grad.txt gradorig.txt costJb.txt costJoJc.txt'
+    os.system(cmd)
 
-def plot(forx,value,iters,VAR,expname): 
+    alist = open(file_name).read().split()
+    iters  = numpy.asarray(alist[0::5])  
+    cost = numpy.asarray(alist[1::5]).astype(np.float32)
+    grad = numpy.asarray(alist[2::5]).astype(np.float32)
+    costJb = numpy.asarray(alist[3::5]).astype(np.float32)
+    costJoJc = numpy.asarray(alist[4::5]).astype(np.float32)
+
+    forx=integers(1,len(iters)) 
+
+    # plot(forx,cost,iters,VAR1,'cost')
+    # plot(forx,grad,iters,VAR2,'grad')
+
+    plot_in_one_figure(forx, cost, costJb, costJoJc, grad, iters)
+
+def plot_in_one_figure(forx, cost, costJb, costJoJc, grad, iters):
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('Iterations',fontsize=12)
+    ax1.set_xticks(forx[::10])
+    ax1.set_xticklabels(iters.astype(np.int32)[::10]) #,rotation=45)
+    ax1.set_ylabel('cost function',fontsize=12)
+    plt.plot(forx,cost,'r.-', label='J')
+    plt.plot(forx,costJb,'b.-', label='Jb')
+    plt.plot(forx,costJoJc,'k.-', label='Jo')
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,4))
+    ax1.legend()
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('gradient norm reduction',fontsize=16)
+    ax2.plot(forx, grad, 'r.:', label='grad')
+    #ax2.legend()
+
+    plt.grid(True)
+    plt.savefig('costgrad.png',dpi=200,bbox_inches='tight')
+    plt.close()
+
+def plot(forx,value,iters,VAR,name): 
     fig, ax1 = plt.subplots()
 
     ax1.set_xlabel('Iterations',fontsize=16)
     ax1.set_xticks(forx[::2])
-    ax1.set_xticklabels(iters.astype(np.int)[::2]) #,rotation=45)
+    ax1.set_xticklabels(iters.astype(np.int32)[::2]) #,rotation=45)
     ax1.set_ylabel('%s'%VAR,fontsize=16)
     plt.plot(forx,value,'r.-')
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0,4))
     plt.grid(True)
-    plt.savefig('%s_%s.png'%(VAR[:4],expname[:-4]),dpi=200,bbox_inches='tight')
+    plt.savefig('%s.png'%(name),dpi=200,bbox_inches='tight')
     plt.close()
    
 def main():
