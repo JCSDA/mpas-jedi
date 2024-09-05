Fix the issue of 3D pressure calculation of the initial condition in graphic package (#1012)
TYPE: Bugfix

DESCRIPTION OF CHANGES:
After switching the MPAS-A model to v8.2.1, the mpas_init_atmosphere doesn't output some variables in the initial condition files. It could be resolved by specifying those variables in the stream file. In case 'pressure' is available while 'pressure_base' and 'pressure_p' are not available, the getPressure function in the graphic package should be modified correspondingly.

Below shows how to add additional variables to the initial conditions in the stream file, which is from @mgduda  
```
<stream name="jedi_ics"
                  type="output"
                  precision="single"
                  clobber_mode="truncate"
                  filename_template="x1.nCells.init.$Y-$M-$D_$h.$m.$s.nc"
                  io_type="pnetcdf,cdf5"
                  output_interval="initial_only" >

        <stream name="output" />
        <var name="pressure" />
        <var name="uReconstructZonal" />
        <var name="uReconstructMeridional" />
</stream>
``` 

LIST OF MODIFIED FILES:
graphics/modelsp_utils.py

TESTS CONDUCTED:
Tested in cycled DA on Derecho

graphics/modelsp_utils.py
@@ -195,9 +195,14 @@ def varDatatype(varName, ncData):
   return ncData.variables[varName].datatype
 
 def getPressure(ncData):
-  pressure_p = np.array( ncData.variables['pressure_p'][0,:,:] )
-  pressure_base = np.array( ncData.variables['pressure_base'][0,:,:] )
-  pressure = pressure_p + pressure_base
+  if 'pressure' in ncData.variables:
+    pressure = np.array( ncData.variables['pressure'][0,:,:] )
+  elif 'pressure_p' in ncData.variables and 'pressure_base' in ncData.variables:
+    pressure_p = np.array( ncData.variables['pressure_p'][0,:,:] )
+    pressure_base = np.array( ncData.variables['pressure_base'][0,:,:] )
+    pressure = pressure_p + pressure_base
+  else:
+    print("Error: 'pressure', 'pressure_p', or 'pressure_base' variables are missing from the NetCDF data.")
   return pressure
 
 def getTemperature(ncData):