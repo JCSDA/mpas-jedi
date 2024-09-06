b5c7d690706d6a8eae8909c5d4086efafd76cd1e
Jim W <70100781+jim-p-w@users.noreply.github.com>
2024-09-06 11:49:04 -0600

    Fix SpawnAnalyzeStats command line processing. (#1005)
    TYPE: bugfix.
    The -s and -q command line arguments to SpawnAnalyzeStats don't work:
    
    That is because the analyzeStatsArgs object (the command line args for AnalyzeStats) is getting loaded via
    import AnalyzeStats as mainScript in graphics/SpawnAnalyzeStats.py, and the args -s and -q don't exist in analyzeStatsArgs.
    
    The fix is to only run ProcessArgs.processArgs for the first instance of a ProcessArgs class.
    
    LIST OF MODIFIED FILES: 
    graphics/ProcessArgs.py
    
    TESTS CONDUCTED: 
    I ran 
    ```
    ./SpawnAnalyzeStats.py -nout 2 -d sfc -s `pwd`
    ```
    and verified the correct graphs were generated.
    I also ran 
     - DiagnoseModelStatistics.py
     - DiagnoseObsStatistics.py
      - GenerateABEIFactors.py
     -  PlotModelSpread.py
    with diagnostic prints to verify those program's argument processors loaded and ran.

--- graphics/ProcessArgs.py
+++ graphics/ProcessArgs.py
@@ -3,18 +3,31 @@
 import argparse
 
 class ProcessArgs:
+  # make the parser a class variable, to limit running processArgs to only one Args object.
+  parser = None
+
   def __init__(self):
     self.argProcessors = [self]
-    self.parser = argparse.ArgumentParser()
+    # guarantee the args data member is defined, even if processArgs doesn't initialize args.
+    self.args = None
 
   def processArgs(self):
-    for processor in self.argProcessors:
-      processor.add_arguments(self.parser)
+    '''
+    Only process one Args parser.
+    Some programs load more than one args parser,
+    due to importing files which in turn import arg parsers.
+    This assumes the desired arg parser is imported first.
+    The main program should explicitly import the Args class it needs.
+    '''
+    if ProcessArgs.parser is None:
+      ProcessArgs.parser = argparse.ArgumentParser()
+      for processor in self.argProcessors:
+        processor.add_arguments(self.parser)
 
-    self.args = self.parser.parse_args()
+      self.args = self.parser.parse_args()
 
-    for processor in self.argProcessors:
-      processor.parse_args(self.args)
+      for processor in self.argProcessors:
+        processor.parse_args(self.args)
 
   @staticmethod
   def add_arguments(parser):
