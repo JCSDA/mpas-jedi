c1cfa7126a14b71c059c40998ff6eb4550c64977
Ivette Hernández Baños <33061897+ibanos90@users.noreply.github.com>
2025-02-14 17:59:53 -0700

    Plotting time series of VarBC predictor coefficients and coefficients errors for all sensors (#1041)
    TYPE: enhancement
    
    DESCRIPTION OF CHANGES: 
    This PR adds the capability to plot the time series of VarBC predictor coefficients and coefficients errors for all sensors provided in `VarBC_dict.py`. This script provides the list of channels used (similar to `config.py`). In that regard, note that I kept channel 3 for MHS from NOAA-19 given that my testing experiment uses ncdiag files, but in recent configurations using raw observations this channel is excluded. Also, note that all predictors are plotted together for each channel and the list of predictors is hard coded including the predictors we are currently using (i.e., 'constant', 'emissivityJacobian', 'lapseRate', 'lapseRate_order_2', 'sensorScanAngle', 'sensorScanAngle_order_2', 'sensorScanAngle_order_3', 'sensorScanAngle_order_4'). In addition, be aware that this script looks for an experiment located in the user's scratch space under the `pandac` folder following the folder structure of the MPAS-Workflow.
    
    To use this script:
    `python VarBC_timeSeries.py -exp EXPNAME -expName shortEXPNAME -dateIni DATEINI -dateEnd DATEEND`
    
    ISSUE: None
    
    LIST OF MODIFIED FILES:
    A       graphics/standalone/VarBC_timeSeries.py
    A       graphics/standalone/VarBC_dict.py
    
    TESTS CONDUCTED:
    Tests were conducted on Derecho and checked against Junmei's script for AMSUA and MHS sensors (Thanks @junmeiban!)

---
+++ graphics/standalone/VarBC_dict.py
@@ -0,0 +1,152 @@
+VarBCDict = {
+    'abi_g16': {
+        'name': 'abi_g16',
+        'channels': range(7,17),
+        'analyzed channels': [8, 9, 10],
+    },
+    'ahi_himawari8': {
+        'name': 'ahi_himawari8',
+        'channels': range(7,17),
+        'analyzed channels': [8, 9, 10],
+    },
+    'abi-clr_g16': {
+        'name': 'abi_g16',
+        'channels': range(7,17),
+        'analyzed channels': [8, 9, 10],
+    },
+    'ahi-clr_himawari8': {
+        'name': 'ahi_himawari8',
+        'channels': range(7,17),
+        'analyzed channels': [8, 9, 10],
+    },
+    'amsua_aqua': {
+        'name': 'amsua_aqua',
+        'channels': range(1,15),
+        'analyzed channels': [8, 9],
+    },
+    'amsua_metop-a': {
+        'name': 'amsua_metop-a',
+        'channels': range(1,15),
+        'analyzed channels': [5, 6, 9],
+    },
+    'amsua_metop-b': {
+        'name': 'amsua_metop-b',
+        'channels': range(1,15),
+        'analyzed channels': [8, 9],
+    },
+    'amsua_n15': {
+        'name': 'amsua_n15',
+        'channels': range(1,15),
+        'analyzed channels': [5, 7, 8, 9],
+    },
+    'amsua_n18': {
+        'name': 'amsua_n18',
+        'channels': range(1,15),
+        'analyzed channels': [5, 6, 7, 8, 9],
+    },
+    'amsua_n19': {
+        'name': 'amsua_n19',
+        'channels': range(1,15),
+        'analyzed channels': [5, 6, 7, 9],
+    },
+    'amsua-cld_aqua': {
+        'name': 'amsua-cld_aqua',
+        'channels': range(1,15),
+        'analyzed channels': [1, 2, 3, 4, 15],
+    },
+    'amsua-cld_metop-a': {
+        'name': 'amsua-cld_metop-a',
+        'channels': range(1,15),
+        'analyzed channels': [1, 2, 3, 4, 15],
+    },
+    'amsua-cld_metop-b': {
+        'name': 'amsua-cld_metop-b',
+        'channels': range(1,15),
+        'analyzed channels': [1, 2, 3, 4],
+    },
+    'amsua-cld_n15': {
+        'name': 'amsua-cld_n15',
+        'channels': range(1,15),
+        'analyzed channels': [1, 2, 3, 4, 15],
+    },
+    'amsua-cld_n18': {
+        'name': 'amsua-cld_n18',
+        'channels': range(1,15),
+        'analyzed channels': [1, 2, 3, 4, 15],
+    },
+    'amsua-cld_n19': {
+        'name': 'amsua-cld_n19',
+        'channels': range(1,15),
+        'analyzed channels': [1, 2, 3, 4, 15],
+    },
+    'mhs_metop-a': {
+        'name': 'mhs_metop-a',
+        'channels': range(1,6),
+        'analyzed channels': [3, 4, 5]
+    },
+    'mhs_metop-b': {
+        'name': 'mhs_metop-b',
+        'channels': range(1,6),
+        'analyzed channels': [3, 4, 5]
+    },
+    'mhs_n18': {
+        'name': 'mhs_n18',
+        'channels': range(1,6),
+        'analyzed channels': [3, 4, 5]
+    },
+    'mhs_n19': {
+        'name': 'mhs_n19',
+        'channels': range(1,6),
+        'analyzed channels': [3, 4, 5]
+    },
+    'cris-fsr_npp': {
+        'name': 'cris-fsr_npp',
+        'channels': [24,26,28,32,37,39],
+        'analyzed channels': [24,26,28,32,37,39],
+    },
+    'hirs4_metop-a': {
+        'name': 'hirs4_metop-a',
+        'channels': range(1,16),
+        'analyzed channels': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    },
+    'iasi_metop-a': {
+        'name': 'iasi_metop-a',
+        'channels': [16,104,154,254,410,1585,2019,2993,3110],
+        'analyzed channels': [16,104,154,254,410,1585,2019,2993,3110],
+    },
+    'iasi_metop-b': {
+        'name': 'iasi_metop-b',
+        'channels': [16,104,154,254,410,1585,2019,2993,3110],
+        'analyzed channels': [16,104,154,254,410,1585,2019,2993,3110],
+    },
+    'iasi_metop-c': {
+        'name': 'iasi_metop-c',
+        'channels': [16,104,154,254,410,1585,2019,2993,3110],
+        'analyzed channels': [16,104,154,254,410,1585,2019,2993,3110],
+    },
+    'seviri_m08': {
+        'name': 'seviri_m08',
+        'channels': [5],
+        'analyzed channels': [5],
+    },
+    'sndrd1_g15': {
+        'name': 'sndrd1_g15',
+        'channels': range(1,16),
+        'analyzed channels': [8, 9, 10],
+    },
+    'sndrd2_g15': {
+        'name': 'sndrd2_g15',
+        'channels': range(1,16),
+        'analyzed channels': [8, 9, 10],
+    },
+    'sndrd3_g15': {
+        'name': 'sndrd3_g15',
+        'channels': range(1,16),
+        'analyzed channels': [8, 9, 10],
+    },
+    'sndrd4_g15': {
+        'name': 'sndrd4_g15',
+        'channels': range(1,16),
+        'analyzed channels': [8, 9, 10],
+    },
+}
---
+++ graphics/standalone/VarBC_timeSeries.py
@@ -0,0 +1,127 @@
+#############################################################################################
+#
+## Script to plot the time series of VarBC predictor coefficients and coefficients errors
+#
+# Usage:
+#
+# python VarBC_timeSeries.py -exp EXP -expName shortEXPNAME -dateIni DATEINI -dateEnd DATEEND
+#############################################################################################
+
+import numpy as np
+import pandas as pd
+import time, os, argparse
+import h5py as h5
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from datetime import datetime, timedelta
+from pathlib import Path
+from VarBC_dict import VarBCDict
+
+def plot(days, sensor, data, expname, typ, title, channel):
+
+    time = pd.to_datetime(days, format='%Y-%m-%d %H:%M:%S')
+
+    fig = plt.figure()
+    gs = GridSpec(ncols=1, nrows=1, height_ratios=[1], wspace=0, hspace=0)
+    ax1 = fig.add_subplot(gs[0])
+
+    predictors = list(data.keys())
+
+    ax1.set_title(title+'\n', loc='center', fontweight='bold')
+    ax1.set_title('Sensor: '+sensor, loc='left')
+    ax1.xaxis.grid(linestyle=":", alpha=0.2, color='grey')
+    ax1.yaxis.grid(linestyle=":", alpha=0.2, color='grey')
+
+    ax1.set_xticks(time[::8])  # every 8 analysis (2 days)
+    ax1.set_xticklabels([date.strftime('%d') for date in time[::8]])
+
+    for pred in predictors:
+      if len(data[pred]) > 0:
+        ax1.plot(time, data[pred],  ls='-', label=pred)
+        ax1.legend(loc="best",framealpha=0, ncol=1)
+        ax1.text(x=0.94, y =-0.12, s=time[-1].strftime('%Y')+'-'+time[-1].strftime('%b'), transform=ax1.transAxes)
+
+    folder_name = 'predictors'
+    folder_path = Path(folder_name)
+    folder_path.mkdir(parents=True, exist_ok=True)
+    fig.savefig(folder_name+'/'+expname+'_'+typ+'_'+sensor+'_'+str(channel)+'.png', dpi=300, format='png', bbox_inches='tight')
+    plt.close(fig)
+
+def main(main_path, dateIni, dateEnd, exp, expname, sensorSat, prefix):
+    h0 = time.time()
+
+    nchans = sensorSat['channels']
+    chans = sensorSat['analyzed channels']
+
+    for c in nchans:
+      if c in chans:
+        print(sensorSat['name'], 'channel: ', c)
+        data_coeff = {v: [] for v in predlist}
+        data_cov   = {v: [] for v in predlist}
+        delta='6'
+        datei = datetime.strptime(str(dateIni), "%Y%m%d%H")
+        datef = datetime.strptime(str(dateEnd), "%Y%m%d%H")
+        date  = datei
+        date_list  = []
+
+        while (date <= datef):
+          datestr  = date.strftime("%Y%m%d%H")
+          day  = date.strftime("%d")
+          date_list.append(date)
+
+          datadir      = main_path+exp+'/CyclingDA/'+datestr+'/dbOut/'
+          ss = sensorSat['name']
+          satbias_file = 'satbias_'+ss+'.h5'
+          satbias_cov_file = 'satbias_cov_'+ss+'.h5'
+          obsout = 'obsout_da_'+prefix+'.h5'
+          if os.path.exists(datadir+obsout):
+            if os.path.exists(datadir+satbias_file) and os.path.exists(datadir+satbias_cov_file):
+              coeff = h5.File(datadir+satbias_file, "r")['BiasCoefficients']
+              cov   = h5.File(datadir+satbias_cov_file, "r")['BiasCoefficientErrors']
+
+              for fp in range(len(predlist)):
+                # this is because we specify channels differently for ABI/AHI/MHS (a subset)
+                if ('abi' in prefix) or ('ahi' in prefix) or ('mhs' in prefix):
+                  ind = chans.index(c)
+                  data_coeff[predlist[fp]] = np.append(data_coeff[predlist[fp]], coeff[predlist[fp]][0][ind])
+                  data_cov[predlist[fp]]   = np.append(data_cov[predlist[fp]],   cov[predlist[fp]][0][ind])
+                else:
+                  data_coeff[predlist[fp]] = np.append(data_coeff[predlist[fp]], coeff[predlist[fp]][0][c-1])
+                  data_cov[predlist[fp]]   = np.append(data_cov[predlist[fp]],   cov[predlist[fp]][0][c-1])
+
+
+          date = date + timedelta(hours=int(delta))
+        non_empty = all(len(data_coeff.get(fp, [])) > 0 and len(data_cov.get(fp, [])) > 0 for fp in predlist)
+
+        if non_empty:
+          plot(date_list, ss, data_coeff, expname, 'predcoeff', 'Bias coefficients @ch'+str(c), c)
+          plot(date_list, ss, data_cov, expname, 'predcov', 'Bias coefficients errors @ch'+str(c), c)
+
+    hf = time.time()
+    print('Time elapsed: ',hf  - h0)
+
+
+if __name__ == '__main__':
+
+   parser = argparse.ArgumentParser(description='Plot time series of VarBC predictor coefficients and coefficients errors', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+   required = parser.add_argument_group(title='required arguments')
+   required.add_argument('-exp', '--exp',type=str, help='Experiment name', required=True)
+   required.add_argument('-expName', '--expName',type=str, help='Experiment short name for convenience', required=True)
+   required.add_argument('-dateIni', '--dateIni',type=str, help='First analysis time', required=True)
+   required.add_argument('-dateEnd', '--dateEnd',type=str, help='Last analysis time', required=True)
+   args = parser.parse_args()
+
+   exp = args.exp
+   shortName = args.expName
+   dateIni   = args.dateIni
+   dateEnd   = args.dateEnd
+
+   user = os.environ['USER']
+   main_path = '/glade/derecho/scratch/'+user+'/pandac/'
+
+   predlist = ['constant', 'emissivityJacobian', 'lapseRate', 'lapseRate_order_2', 'sensorScanAngle', 'sensorScanAngle_order_2', 'sensorScanAngle_order_3', 'sensorScanAngle_order_4']
+
+   obsPrefix = VarBCDict.keys()
+
+   for prefix in obsPrefix:
+     main(main_path, dateIni, dateEnd, exp, shortName, VarBCDict[prefix], prefix)
