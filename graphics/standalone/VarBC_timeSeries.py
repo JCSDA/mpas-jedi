6c99469c17df14587026b0f55e735be449304fc1
Ivette Hernández Baños <33061897+ibanos90@users.noreply.github.com>
2025-05-14 10:56:54 -0600

    Add option for MHS obs type and remove emissivityJacobian if desired. (#1062)
    TYPE: choose one of [enhancement]
    
    DESCRIPTION OF CHANGES: 
    This PR extends the plotting script for VarBC_timeSeries to plot raw MHS (originally only for GSI-ncdiag MHS obs) and to (optionally) remove emissivity Jacobian from the predictors list (for convenience). Default options are raw MHS obs and including emissivity Jacobian. I also added parallelization to make all sensors at once, which reduces considerably the runtime.
    
    Usage to activate new options:
    ```
    python VarBC_timeSeries.py -exp EXP -expName shortEXPNAME -dateIni DATEINI -dateEnd DATEEND -mhsType 'ncdiag' -removeEmiss True
    ```
    
    LIST OF MODIFIED FILES:
    M       graphics/standalone/VarBC_timeSeries.py
    
    TESTS CONDUCTED:
    Tested on Derecho for various experiments results including benchmark experiment with raw MHS obs and previous experiments with GSI-ncdiag MHS obs.

--- graphics/standalone/VarBC_timeSeries.py
+++ graphics/standalone/VarBC_timeSeries.py
@@ -1,26 +1,29 @@
-#############################################################################################
+#################################################################################################################################
 #
 ## Script to plot the time series of VarBC predictor coefficients and coefficients errors
 #
 # Usage:
 #
-# python VarBC_timeSeries.py -exp EXP -expName shortEXPNAME -dateIni DATEINI -dateEnd DATEEND
-#############################################################################################
+# python VarBC_timeSeries.py -exp EXP -expName shortEXPNAME -dateIni DATEINI -dateEnd DATEEND -mhsType 'ncdiag' -removeEmiss True
+#################################################################################################################################
 
 import numpy as np
 import pandas as pd
 import time, os, argparse
 import h5py as h5
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from datetime import datetime, timedelta
 from pathlib import Path
 from VarBC_dict import VarBCDict
 import math
+from concurrent.futures import ProcessPoolExecutor
 
-def plot(days, sensor, data, expname, typ, title, channel, nobs):
+def plot(date_list, sensor, data, expname, typ, title, channel, nobs):
 
-    time = pd.to_datetime(days, format='%Y-%m-%d %H:%M:%S')
+    time = pd.to_datetime(date_list, format='%Y%m%d%H')
 
     fig = plt.figure()
     gs = GridSpec(ncols=1, nrows=1, height_ratios=[1], wspace=0, hspace=0)
@@ -29,105 +32,113 @@ def plot(days, sensor, data, expname, typ, title, channel, nobs):
     predictors = list(data.keys())
 
     ax1.set_title(title+'\n', loc='center', fontweight='bold')
-    ax1.set_title('Sensor: '+sensor+'  nObsUsed @last cycle='+str(nobs) , loc='left')
+    ax1.set_title('Sensor: '+sensor, loc='left')
+    ax1.set_title('nObsUsed @last cycle='+str(nobs), loc='right')
     ax1.xaxis.grid(linestyle=":", alpha=0.2, color='grey')
     ax1.yaxis.grid(linestyle=":", alpha=0.2, color='grey')
 
     ax1.set_xticks(time[::8])  # every 8 analysis (2 days)
     ax1.set_xticklabels([date.strftime('%d') for date in time[::8]])
 
-    if (typ == 'predcov'):
-      ax1.set_ylabel('Standard Deviation',fontsize=12)
-    if (typ == 'predcoeff'):
-      ax1.set_ylabel('Beta',fontsize=12)
+    ax1.set_ylabel('Standard Deviation' if typ == 'predcov' else 'Beta', fontsize=12)
 
     for pred in predictors:
-      if len(data[pred]) > 0:
-        ax1.plot(time, data[pred],  ls='-', label=pred)
-        ax1.legend(loc="best",framealpha=0, ncol=1)
-        ax1.text(x=0.94, y =-0.12, s=time[-1].strftime('%Y')+'-'+time[-1].strftime('%b'), transform=ax1.transAxes)
+        if len(data[pred]) > 0:
+          ax1.plot(time, data[pred], ls='-', label=pred)
+
+    ax1.legend(loc="best", framealpha=0, ncol=1)
+    ax1.text(x=0.94, y=-0.12, s=time[-1].strftime('%Y-%b'), transform=ax1.transAxes)
 
     folder_name = 'predictors'
-    folder_path = Path(folder_name)
-    folder_path.mkdir(parents=True, exist_ok=True)
-    fig.savefig(folder_name+'/'+expname+'_'+typ+'_'+sensor+'_'+str(channel)+'.png', dpi=300, format='png', bbox_inches='tight')
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
+    fig.savefig(f'{folder_name}/{expname}_{typ}_{sensor}_{channel}.png', dpi=300, format='png', bbox_inches='tight')
     plt.close(fig)
 
-def main(main_path, dateIni, dateEnd, exp, expname, sensorSat, prefix):
+def main(args_tuple):
     h0 = time.time()
+
+    main_path, date_list, exp, expname, sensorSat, prefix, mhsType, removeEmiss = args_tuple
 
     nchans = sensorSat['channels']
     chans = sensorSat['analyzed channels']
+    n_dates = len(date_list)
 
     for c in nchans:
-      if c in chans:
+        if c not in chans:
+            continue
         print(sensorSat['name'], 'channel: ', c)
-        data_coeff = {v: [] for v in predlist}
-        data_cov   = {v: [] for v in predlist}
-        delta='6'
-        datei = datetime.strptime(str(dateIni), "%Y%m%d%H")
-        datef = datetime.strptime(str(dateEnd), "%Y%m%d%H")
-        date  = datei
-        date_list  = []
 
-        while (date <= datef):
-          datestr  = date.strftime("%Y%m%d%H")
-          day  = date.strftime("%d")
-          date_list.append(date)
+        ind = chans.index(c)
+        ss = sensorSat['name']
+        data_coeff, data_cov, predlist = {}, {}, None
 
-          datadir      = main_path+exp+'/CyclingDA/'+datestr+'/dbOut/'
-          ss = sensorSat['name']
-          satbias_file = 'satbias_'+ss+'.h5'
-          satbias_cov_file = 'satbias_cov_'+ss+'.h5'
-          obsout = 'obsout_da_'+prefix+'.h5'
-          if os.path.exists(datadir+obsout):
-            if os.path.exists(datadir+satbias_file) and os.path.exists(datadir+satbias_cov_file):
-              coeff = h5.File(datadir+satbias_file, "r")['BiasCoefficients']
-              cov   = h5.File(datadir+satbias_cov_file, "r")['BiasCoefficientErrors']
-              nobs  = h5.File(datadir+satbias_cov_file, "r")['numberObservationsUsed']
+        for date_idx, datestr in enumerate(date_list):
+            datadir = main_path+exp+'/CyclingDA/'+datestr+'/dbOut/'
+            ss = sensorSat['name']
+            satbias_file = 'satbias_'+ss+'.h5'
+            satbias_cov_file = 'satbias_cov_'+ss+'.h5'
+            obsout = 'obsout_da_'+prefix+'.h5'
 
-              for fp in range(len(predlist)):
-                # this is because we specify channels differently for ABI/AHI/MHS (a subset)
-                if ('abi' in prefix) or ('ahi' in prefix) or ('mhs' in prefix):
-                  ind = chans.index(c)
-                  data_coeff[predlist[fp]] = np.append(data_coeff[predlist[fp]], coeff[predlist[fp]][0][ind])
-                  data_cov[predlist[fp]]   = np.append(data_cov[predlist[fp]],   cov[predlist[fp]][0][ind])
-                else:
-                  data_coeff[predlist[fp]] = np.append(data_coeff[predlist[fp]], coeff[predlist[fp]][0][c-1])
-                  data_cov[predlist[fp]]   = np.append(data_cov[predlist[fp]],   math.sqrt(cov[predlist[fp]][0][c-1]))
+            if not all(os.path.exists(os.path.join(datadir, f)) for f in [obsout, satbias_file, satbias_cov_file]):
+                continue
 
-          date = date + timedelta(hours=int(delta))
-        non_empty = all(len(data_coeff.get(fp, [])) > 0 and len(data_cov.get(fp, [])) > 0 for fp in predlist)
+            with h5.File(os.path.join(datadir, satbias_file), "r") as f_coeff, \
+                 h5.File(os.path.join(datadir, satbias_cov_file), "r") as f_cov:
+                coeff = f_coeff['BiasCoefficients']
+                cov = f_cov['BiasCoefficientErrors']
+                nobs_ = f_cov['numberObservationsUsed']
 
-        if non_empty:
-          plot(date_list, ss, data_coeff, expname, 'predcoeff', 'Bias coefficients @ch'+str(c), c, nobs[0][c-1])
-          plot(date_list, ss, data_cov, expname, 'predcov', 'Bias coefficients errors @ch'+str(c), c, nobs[0][c-1])
+                if predlist is None:
+                    predlist = [k for k in coeff.keys() if not removeEmiss or k != 'emissivityJacobian']
+                    data_coeff = {pred: np.full(n_dates, np.nan) for pred in predlist}
+                    data_cov = {pred: np.full(n_dates, np.nan) for pred in predlist}
 
-    hf = time.time()
-    print('Time elapsed: ',hf  - h0)
+                for pred in predlist:
+                    # this is because we specify channels differently for ABI/AHI/MHS-ncdiag (a subset)
+                    if (prefix.startswith('abi') or prefix.startswith('ahi') or
+                        (prefix.startswith('mhs') and mhsType == 'ncdiag')):
+                        data_coeff[pred][date_idx] = coeff[pred][0][ind]
+                        data_cov[pred][date_idx] = math.sqrt(cov[pred][0][ind])
+                        nobs = nobs_[0][ind]
+                    else:
+                        # here c-1 to extract the correct predictor value for the specific channel
+                        # as indices in python start at zero
+                        data_coeff[pred][date_idx] = coeff[pred][0][c-1]
+                        data_cov[pred][date_idx] = math.sqrt(cov[pred][0][c-1])
+                        nobs = nobs_[0][c-1]
 
+        if predlist and any(~np.isnan(data_coeff[pred]).all() for pred in predlist):
+           plot(date_list, ss, data_coeff, expname, 'predcoeff', 'Bias coefficients @ch'+str(c), c, nobs)
+           plot(date_list, ss, data_cov, expname, 'predcov', 'Bias coefficients errors @ch'+str(c), c, nobs)
+
+    print(f'[DONE] {prefix} in {time.time() - h0:.2f} seconds')
 
 if __name__ == '__main__':
+    h = time.time()
 
-   parser = argparse.ArgumentParser(description='Plot time series of VarBC predictor coefficients and coefficients errors', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-   required = parser.add_argument_group(title='required arguments')
-   required.add_argument('-exp', '--exp',type=str, help='Experiment name', required=True)
-   required.add_argument('-expName', '--expName',type=str, help='Experiment short name for convenience', required=True)
-   required.add_argument('-dateIni', '--dateIni',type=str, help='First analysis time', required=True)
-   required.add_argument('-dateEnd', '--dateEnd',type=str, help='Last analysis time', required=True)
-   args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Plot time series of VarBC predictor coefficients and coefficients errors', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-exp', '--exp', type=str, help='Experiment name', required=True)
+    parser.add_argument('-expName', '--expName', type=str, help='Experiment short name for convenience', required=True)
+    parser.add_argument('-dateIni', '--dateIni', type=str, help='First analysis time', required=True)
+    parser.add_argument('-dateEnd', '--dateEnd', type=str, help='Last analysis time', required=True)
+    parser.add_argument('-mhsType',  '--mhsType', type=str, help='MHS data type: raw or ncdiag', default='raw')
+    parser.add_argument('-removeEmiss', '--removeEmiss', type=bool, help='Remove emissivity Jacobian from predictors list', default=False)
+    args = parser.parse_args()
 
-   exp = args.exp
-   shortName = args.expName
-   dateIni   = args.dateIni
-   dateEnd   = args.dateEnd
+    user = os.environ['USER']
+    main_path = '/glade/derecho/scratch/'+user+'/pandac/'
 
-   user = os.environ['USER']
-   main_path = '/glade/derecho/scratch/'+user+'/pandac/'
+    delta = 6
+    start = datetime.strptime(args.dateIni, "%Y%m%d%H")
+    end = datetime.strptime(args.dateEnd, "%Y%m%d%H")
+    date_list = [(start + timedelta(hours=i)).strftime("%Y%m%d%H")
+                 for i in range(0, int((end - start).total_seconds() // 3600) + 1, delta)]
 
-   predlist = ['constant', 'emissivityJacobian', 'lapseRate', 'lapseRate_order_2', 'sensorScanAngle', 'sensorScanAngle_order_2', 'sensorScanAngle_order_3', 'sensorScanAngle_order_4']
+    tasks = []
+    for prefix in VarBCDict:
+        tasks.append((main_path, date_list, args.exp, args.expName, VarBCDict[prefix], prefix, args.mhsType, args.removeEmiss))
 
-   obsPrefix = VarBCDict.keys()
+    with ProcessPoolExecutor() as executor:
+        executor.map(main, tasks)
 
-   for prefix in obsPrefix:
-     main(main_path, dateIni, dateEnd, exp, shortName, VarBCDict[prefix], prefix)
+    print(f'Total time elapsed: {time.time() - h:.2f} seconds')
