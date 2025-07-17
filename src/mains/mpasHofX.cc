19c1931170000aafa8a98d6f9203a36a849d5343
Clementine Hardy Gas <43183478+cmgas@users.noreply.github.com>
2025-07-17 14:56:18 -0600

    Add obs error factory instantiation to hofx (#1078)
    TYPE: bug fix
    
    DESCRIPTION OF CHANGES: The obs error factory was not instantiated in hofx (but was in hofx 3d). This means the obs error could only be diagonal, and users couldn't run with `cross variable covariances` for example. 
    This just adds the instantiation of the factory in the main. 
    
    LIST OF MODIFIED FILES:
    M       src/mains/mpasHofX.cc
    
    TESTS CONDUCTED: ctests in mpas-jedi, hofx with `cross variable covariances` in skylab

--- src/mains/mpasHofX.cc
+++ src/mains/mpasHofX.cc
@@ -1,14 +1,14 @@
 /*
  * (C) Copyright 2017 UCAR
- * 
+ *
  * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
 #include "oops/generic/instantiateModelFactory.h"
 #include "oops/runs/HofX4D.h"
 #include "oops/runs/Run.h"
-
+#include "ufo/instantiateObsErrorFactory.h"
 #include "ufo/instantiateObsFilterFactory.h"
 #include "ufo/ObsTraits.h"
 
@@ -17,6 +17,7 @@
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
   oops::instantiateModelFactory<mpas::Traits>();
+  ufo::instantiateObsErrorFactory();
   ufo::instantiateObsFilterFactory();
   oops::HofX4D<mpas::Traits, ufo::ObsTraits> hofx;
   return run.execute(hofx);
